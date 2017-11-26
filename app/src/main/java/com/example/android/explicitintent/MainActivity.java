e3be53b78c3eb0e12517a6eca23020ab146736fa
coffeeplanter <is3k@ya.ru>
2017-11-26 15:29:29 +0300

    Exercise 4a.3 done.

--- app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -17,8 +17,8 @@ package com.example.android.explicitintent;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -40,8 +40,8 @@ public class MainActivity extends AppCompatActivity {
          * do things like set the onClickListener which determines what happens when the button
          * is clicked.
          */
-        mDoSomethingCoolButton = (Button) findViewById(R.id.b_do_something_cool);
-        mNameEntry = (EditText) findViewById(R.id.et_text_entry);
+        mDoSomethingCoolButton = findViewById(R.id.b_do_something_cool);
+        mNameEntry = findViewById(R.id.et_text_entry);
 
         /* Setting an OnClickListener allows us to do something when this button is clicked. */
         mDoSomethingCoolButton.setOnClickListener(new OnClickListener() {
@@ -53,7 +53,8 @@ public class MainActivity extends AppCompatActivity {
              */
             @Override
             public void onClick(View v) {
-                // TODO (1) Retrieve the text from the EditText and store it in a variable
+
+                String textEntered = mNameEntry.getText().toString();
 
                 /*
                  * Storing the Context in a variable in this case is redundant since we could have
@@ -73,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
                  */
                 Intent startChildActivityIntent = new Intent(context, destinationActivity);
 
-                // TODO (2) Use the putExtra method to put the String from the EditText in the Intent
+                startChildActivityIntent.putExtra(Intent.EXTRA_TEXT, textEntered);
 
                 /*
                  * Once the Intent has been created, we can use Activity's method, "startActivity"
@@ -83,5 +84,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-}
 
+}
