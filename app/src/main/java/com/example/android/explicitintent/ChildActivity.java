dd09ee4ec3b53098bdd0d3f6cfd1e5b5d884765e
coffeeplanter <is3k@ya.ru>
2017-11-26 15:02:48 +0300

    Exercise 4a.1 done.

---
+++ app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.explicitintent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class ChildActivity extends AppCompatActivity {
+
+    @SuppressWarnings({"FieldCanBeLocal", "unused"})
+    private TextView messageTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+        messageTextView = findViewById(R.id.tv_display);
+    }
+
+}
