f9bce1ffa4daff3d2c48f879d06c2e464ff16786
coffeeplanter <is3k@ya.ru>
2017-12-16 23:25:30 +0300

    Exercise completed.

--- app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -6,6 +6,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     /*
@@ -39,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private TextView mLifecycleDisplay;
 
-    // TODO (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
+    private static List<String> mLifecycleCallbacks = new ArrayList<>();
 
     /**
      * Called when the activity is first created. This is where you should do all of your normal
@@ -54,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mLifecycleDisplay = (TextView) findViewById(R.id.tv_lifecycle_events_display);
+        mLifecycleDisplay = findViewById(R.id.tv_lifecycle_events_display);
 
         /*
          * If savedInstanceState is not null, that means our Activity is not being started for the
@@ -71,9 +74,10 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        // TODO (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
-
-        // TODO (5) Clear mLifecycleCallbacks after iterating through it
+        for (String callback : mLifecycleCallbacks) {
+            mLifecycleDisplay.append(callback + "\n");
+        }
+        mLifecycleCallbacks.clear();
 
         logAndAppend(ON_CREATE);
     }
@@ -133,8 +137,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
 
-        // TODO (2) Add the ON_STOP String to the front of mLifecycleCallbacks
-
+        mLifecycleCallbacks.add(ON_STOP);
         logAndAppend(ON_STOP);
     }
 
@@ -160,8 +163,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
 
-        // TODO (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
-
+        mLifecycleCallbacks.add(ON_DESTROY);
         logAndAppend(ON_DESTROY);
     }
 
@@ -194,4 +196,5 @@ public class MainActivity extends AppCompatActivity {
     public void resetLifecycleDisplay(View view) {
         mLifecycleDisplay.setText("Lifecycle callbacks:\n");
     }
+
 }
